0e6394b0e9ace3f1b25978db7cfe6a4db462326e
daniilyamkavoi <danik.2012@mail.ru>
2020-10-29 19:43:59 -0400

    data folder split test

---
+++ test/smoke/initial.js
@@ -0,0 +1,63 @@
+const sel = require('../../data/selectors.json')
+const exp = require('../../data/expected.json')
+describe('My Little Hero', function () {
+
+    describe('Getting to the page', function () {
+
+        it('TC-001 Title is correct ', function () {
+            browser.url('https://qa-apps.netlify.app/app_my_hero');
+            let title = browser.getTitle();
+            browser.pause(2000);
+            expect(title).toEqual('MLH trial');
+        });
+
+    });
+
+    describe('Labels exist', function () {
+
+        it('TC-002 Label for name', function () {
+            const label = $$(sel.label)[0].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+        it('TC-003 Label for gender', function () {
+            const label = $$(sel.label)[1].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+        it('TC-004 Label for age', function () {
+            const label = $$(sel.label)[2].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+        it('TC-005 Label for story', function () {
+            const label = $$(sel.label)[3].isDisplayed();
+            expect(label).toEqual(true);
+        });
+
+    });
+
+    describe('Labels are correct', function () {
+
+        it('TC-006 Label for name = 1. What is your HERO\'s name?', function () {
+            const text = $$(sel.label)[0].getAttribute('title');
+            expect(text).toEqual(exp.labelName);
+        });
+
+        it('TC-007 Label for gender = 2. Please choose a gender.', function () {
+            const text = $$(sel.label)[1].getAttribute('title');
+            expect(text).toEqual(exp.labelGender);
+        });
+
+        it('TC-008 Label for age = 3. How old is your Hero?', function () {
+            const text = $$(sel.label)[2].getAttribute('title');
+            expect(text).toEqual(exp.labelAge);
+        });
+
+        it('TC-009 Label for story = 4. What type of story would you like to read?', function () {
+            const text = $$(sel.label)[3].getAttribute('title');
+            expect(text).toEqual(exp.labelStory);
+        });
+
+    });
+});
--- wdio.conf.js
+++ wdio.conf.js
@@ -17,7 +17,8 @@ exports.config = {
     // directory is where your package.json resides, so `wdio` will be called from there.
     //
     specs: [
-        './test/*.js'
+        './test/smoke/*.js',
+        './test/regression/*.js'
     ],
     // Patterns to exclude.
     exclude: [
@@ -66,7 +67,7 @@ exports.config = {
     // Define all options that are relevant for the WebdriverIO instance here
     //
     // Level of logging verbosity: trace | debug | info | warn | error | silent
-    logLevel: 'info',
+    logLevel: 'silent',
     //
     // Set specific log levels per logger
     // loggers:
