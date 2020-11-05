dfe7738ba4cca0efecbc30058f346b59f456fe5b
dannywhy <73038718+dannywhy@users.noreply.github.com>
2020-11-04 20:00:09 -0500

    testData and inputValues4 (#3)
    MLH-476

---
+++ helpers/inputValues4.js
@@ -0,0 +1,10 @@
+const sel = require ('../data/selectors.json');
+
+function inputValues4 (name, gender, age, storyType){
+    $(sel.name).setValue(name);
+    $$(sel.gender)[gender].click();
+    $(sel.age).setValue(age);
+    $(sel.story).click();
+    $$(sel.storyType)[storyType].click();
+}
+module.exports = inputValues4;
