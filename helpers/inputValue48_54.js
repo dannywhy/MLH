8fa9208edd933a04abdca8aff405c9c1c1c46dee
Oleh Vakhrushev <63006023+OlehVakhrushev@users.noreply.github.com>
2020-11-09 20:38:02 -0800

    MLH-530 (#13)
    * add data and helpers
    
    * add data and helpers
    
    * MHL-041
    
    * Autotest 234-253
    
    * add func and test 48_54
    
    * add func and test 48_54
    
    Co-authored-by: Oleh V <olehv@Olehs-MacBook-Pro.local>

---
+++ helpers/inputValue48_54.js
@@ -0,0 +1,8 @@
+const sel = require("../data/selectors.json");
+
+function inputValue48_54 (storyType) {
+    $(sel.story).click();
+    $$(sel.storyType)[storyType].click();
+    return $(sel.story).getText();
+}
+module.exports = inputValue48_54;
