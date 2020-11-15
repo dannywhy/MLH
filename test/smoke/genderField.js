71ebc7fb9ddaa67c05b4d5c22f8833628a4e5ecd
Anytka555 <59509500+Anytka555@users.noreply.github.com>
2020-11-14 23:01:41 -0500

    Add nameField file (#21)
    https://jira.pasv.us/browse/MLH-474

---
+++ test/smoke/genderField.js
@@ -0,0 +1,29 @@
+const sel = require ('../../data/selectors.json');
+import {expect} from 'chai';
+
+describe('Gender chooses', function () {
+
+    describe('User can choose gender type', function () {
+
+        it('TC-021 Choose gender (he)', function () {
+            browser.url('');
+            $(sel.gender).click();
+            const value = $(sel.selectedGender).getAttribute('innerHTML');
+            expect(value).to.contain('he');
+        });
+
+        it('TC-022 Choose gender (she)', function () {
+            browser.url('');
+            $$(sel.gender)[1].click();
+            const value = $(sel.selectedGender).getAttribute('innerHTML');
+            expect(value).to.contain('she');
+        });
+
+        it('TC-023 Choose gender (it)', function () {
+            browser.url('');
+            $$(sel.gender)[2].click();
+            const value = $(sel.selectedGender).getAttribute('innerHTML');
+            expect(value).to.contain('it');
+        });
+    });
+});
---
+++ test/smoke/nameField.js
@@ -0,0 +1,45 @@
+const sel = require ('../../data/selectors.json');
+const exp = require ('../../data/expected.json');
+
+describe('Checks for Name Field', function () {
+
+    before(function () {
+        browser.url("");
+        browser.maximizeWindow();
+    });
+
+    afterEach(function () {
+        browser.refresh()
+    });
+
+    it('TC007 Label for name', function () {
+        const label = $(sel.label).getText();
+        expect(label).toEqual(exp.labelName);
+    });
+
+    it('TC008 Input letters', function () {
+        $(sel.name).setValue('Lady');
+        expect($$(sel.fieldClass)[0]).toHaveAttrContaining('class', 'success');
+    });
+
+    it('TC012 Input one letter', function () {
+        $(sel.name).setValue('L');
+        expect($$(sel.fieldClass)[0]).toHaveAttrContaining('class', 'success');
+    });
+
+    it('TC013 Input digits', function () {
+        $(sel.name).setValue('12345');
+        expect($$(sel.fieldClass)[0]).toHaveAttrContaining('class', 'success');
+    });
+
+    it('TC014 Input special characters', function () {
+        $(sel.name).setValue('%$#*');
+        expect($$(sel.fieldClass)[0]).toHaveAttrContaining('class', 'success');
+    });
+
+    it('TC017 Input letters and digits ', function () {
+        $(sel.name).setValue('Lady777');
+        expect($$(sel.fieldClass)[0]).toHaveAttrContaining('class', 'success');
+
+    });
+});
